6d409e894ae4a1b966f5e2c38e1b0eca7d8a6b8f
Douglas Alves <dsilva@ufu.br>
2022-04-01 19:52:06 -0300

    Add files via upload

---
+++ Neps Academy.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  1 19:46:33 2022
+
+@author: fisic
+"""
+
+print ("Neps", end="  ")
+print (" Academy")
---
+++ SOMA.py
@@ -0,0 +1,4 @@
+A= 4
+B= 3
+ 
+print ("\nO valor da soma é {:.2f}".format(A/B))
