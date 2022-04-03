e156bf53db17ec0673b2db5d0b53cfc7cf022e4a
Douglas Alves <dsilva@ufu.br>
2022-04-03 15:08:44 -0300

    Add files via upload

---
+++ CINO E BINO .py
@@ -0,0 +1,18 @@
+Bino = int(input())
+Cino = int(input())
+
+if (((Bino+Cino)% 2) == 0):
+    print("Bino")
+else:
+    print("Cino")
+
+
+"""
+A = int(input())
+B = int(input())
+
+if((A + B) % 2 == 0):
+	print("Bino")
+else:
+	print("Cino")
+"""
